2e9131f822b910f289413713104072b93fb851be
YE <yaakovde123@gmail.com>
2026-02-03 14:35:24 +0200

    solved forth p

---
+++ forth_p.py
@@ -0,0 +1,9 @@
+import os
+
+def recursive_printing(path):
+    name_list = os.listdir(path)
+    print(name_list)
+    for name in name_list:
+        n_path = os.path.join(n_path,name) 
+        if os.path.isdir(n_path):
+            recursive_printing(n_path)
