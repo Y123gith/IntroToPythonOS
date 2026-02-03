9617141637210004cc7b5ccaa8434253cac47b8f
YE <yaakovde123@gmail.com>
2026-02-03 14:55:55 +0200

    solved fith p

---
+++ fith_p.py
@@ -0,0 +1,11 @@
+import os
+
+def return_py_files(path):
+    dir_files = os.listdir(path)
+    for file in dir_files:
+        n_path = os.path.join(path,file) 
+        if os.path.isdir(n_path):
+            return_py_files(n_path)
+        if ".py" in file:
+            print(file)
+    return
