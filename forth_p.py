be0be774124ab5ced1d5c685caa8b90cd62544a6
Y123gith <yaakovde123@gmail.com>
2026-02-03 14:52:51 +0200

    Update forth_p.py

--- forth_p.py
+++ forth_p.py
@@ -4,7 +4,7 @@ def recursive_printing(path):
     name_list = os.listdir(path)
     print(name_list)
     for name in name_list:
-        n_path = os.path.join(n_path,name) 
+        n_path = os.path.join(path,name) 
         if os.path.isdir(n_path):
             recursive_printing(n_path)
     return
